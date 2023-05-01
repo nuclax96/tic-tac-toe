e55161ed2cec0bea76f3d4e5a211c6efec9f4831
nuclax96 <adityarastogi94c@gmail.com>
2023-05-01 21:41:06 +0530

    Logic to check if game won implemented

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 "use-strict";
 
+const objCond = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 const gameBoard = (function () {
   const boardArr = ["", "", "", "", "", "", "", "", ""];
   const display = function () {
@@ -29,7 +40,25 @@ const gameController = (function () {
     gameBoard.display();
   };
 
-  const flipTurn = function () {};
+  const getCurrentPlayerSign = function () {
+    if (player1.turn === true) {
+      return player1.sign;
+    }
+    if (player2.turn === true) {
+      return player2.sign;
+    }
+  };
+
+  const flipTurn = function (currPlayer) {
+    if (currPlayer === 1) {
+      player1.turn = false;
+      player2.turn = true;
+    }
+    if (currPlayer === 2) {
+      player2.turn = false;
+      player1.turn = true;
+    }
+  };
   const isValidMove = function (index) {
     if (gameBoard.boardArr[index] !== "") {
       console.log("Wrong Move");
@@ -38,7 +67,32 @@ const gameController = (function () {
       return true;
     }
   };
-  const checkGameStatus = function () {};
+
+  // Checking the status of the game after each move
+  const checkGameStatus = function () {
+    const tempArr = gameBoard.boardArr;
+    let flag = true;
+    let currentPlayerSign = getCurrentPlayerSign();
+
+    for (const item of objCond) {
+      item.forEach((el) => {
+        if (tempArr[el] === currentPlayerSign) {
+          flag = true;
+        } else {
+          flag = false;
+        }
+      });
+      if (flag === true) {
+        break;
+      }
+    }
+
+    if (flag === true) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 
   const makeMove = function (index) {
     if (!isValidMove(index)) {
@@ -47,16 +101,19 @@ const gameController = (function () {
     if (player1.turn) {
       gameBoard.boardArr[index] = player1.sign;
       gameBoard.markCell(index);
-      checkGameStatus();
-      player1.turn = false;
-      player2.turn = true;
+      const status = checkGameStatus();
+      if (status) {
+        console.log("Won");
+      }
+
+      flipTurn(1);
       return;
     }
     if (player2.turn) {
       gameBoard.boardArr[index] = player2.sign;
       gameBoard.markCell(index);
-      player2.turn = false;
-      player1.turn = true;
+      checkGameStatus();
+      flipTurn(2);
       return;
     }
   };
@@ -64,13 +121,13 @@ const gameController = (function () {
 })();
 
 gameController.startGame();
-gameController.makeMove(1);
-gameController.makeMove(0);
+gameController.makeMove(0); //X
+gameController.makeMove(4); //O
+gameController.makeMove(1); //X
+gameController.makeMove(8); //o
 gameController.makeMove(2);
 gameController.makeMove(3);
-gameController.makeMove(4);
-gameController.makeMove(5);
 gameController.makeMove(6);
 gameController.makeMove(7);
-gameController.makeMove(8);
-console.log(gameBoard.boardArr);
+gameController.makeMove(5);
+// console.log(gameBoard.boardArr);
